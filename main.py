d12743979aba907d7401e62ddf30a23f0810b907
ete <stefanvallet99@gmail.com>
2019-03-01 22:53:16 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+students = []
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+
+@app.route("/register", methods=['POST'])
+def register():
+    name = request.form.get("name")
+    if not name:
+        print("nah")
+    print("yaah")
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+
+    app.debug = True
+    app.run()
+    '''host='192.168.0.13', port=8000'''
---
+++ static/animador1.js
@@ -0,0 +1,12 @@
+function j()
+{
+    //document.write("holas");
+    var h = document.querySelector("h1");
+    h.addEventListener("click", function(){
+        alert("holas");
+    });
+}
+var h = document.querySelector("h1");
+h.addEventListener("click", function(){
+    alert("holas");
+});
